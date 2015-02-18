70c61da3cae5bb371d00117f1635bd118d42d550
Philip Wernersbach <philip.wernersbach@gmail.com>
2015-02-18 00:16:49 -0500

    Start work on authentication system.

--- src/app/controllers/Application.java
+++ src/app/controllers/Application.java
@@ -2,13 +2,27 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.*;
 
 import views.html.*;
+import models.*;
 
 public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render());
+    }
+
+    public static Result login() {
+        return ok(login.render(Form.form(User.class)));
+    }
+
+    public static Result authenticate() {
+    	return internalServerError("Not implemented yet");
+    }
+
+    public static Result register() {
+        return internalServerError("Not implemented yet");
     }
 
 }
