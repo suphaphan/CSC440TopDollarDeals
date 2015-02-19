86c3f8612db990881ad72c1b75cd00469cd36f6d
Philip Wernersbach <philip.wernersbach@gmail.com>
2015-02-18 19:53:02 -0500

    Finish login system.

--- src/app/controllers/Application.java
+++ src/app/controllers/Application.java
@@ -14,15 +14,55 @@ public class Application extends Controller {
     }
 
     public static Result login() {
-        return ok(login.render(Form.form(User.class)));
+        if (session("user_email") == null)
+            return ok(login.render(Form.form(User.class), Form.form(User.class)));
+        else
+            return redirect(routes.Application.index());
+    }
+
+    public static Result doLogin(Form<User> data) {
+        User potentialUser = data.get();
+        User user = User.find.byId(potentialUser.email);
+
+        if (!user.isPassword(potentialUser.password))
+            return badRequest(login.render(data, Form.form(User.class)));
+
+        session().clear();
+        session("user_email", user.email);
+        return redirect(routes.Application.index());
     }
 
     public static Result authenticate() {
-    	return internalServerError("Not implemented yet");
+        Form<User> data = Form.form(User.class).bindFromRequest();
+
+        if (data.hasErrors())
+            return badRequest(login.render(data, Form.form(User.class)));
+        else
+            return doLogin(data);
     }
 
     public static Result register() {
-        return internalServerError("Not implemented yet");
+        User newUser = null;
+        User existingUser = null;
+        Form<User> data = Form.form(User.class).bindFromRequest();
+
+        if (data.hasErrors())
+            return badRequest(login.render(Form.form(User.class), data));
+
+        newUser = data.get();
+
+        if (User.find.byId(newUser.email) != null)
+            return badRequest(login.render(Form.form(User.class), data));
+
+        newUser.changePassword(newUser.password);
+        newUser.save();
+
+        return doLogin(data);
+    }
+
+    public static Result logout() {
+        session().clear();
+        return redirect(routes.Application.index());
     }
 
 }
--- src/app/models/User.java
+++ src/app/models/User.java
@@ -1,12 +1,20 @@
 package models;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.persistence.*;
 import play.data.validation.*;
 import play.db.ebean.*;
 import com.avaje.ebean.*;
 
+import controllers.BCrypt;
+
 @Entity
 public class User extends Model {
+    /* Email regex from http://www.mkyong.com/regular-expressions/how-to-validate-email-address-with-regular-expression/ */
+    private static final String EMAIL_REGEX = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+
     @Id
     public String email;
 
@@ -25,4 +33,20 @@ public class User extends Model {
     public static Finder<String,User> find = new Finder<String,User>(
         String.class, User.class
     );
+
+    public String validate() {
+      Matcher validEmail = EMAIL_PATTERN.matcher(email);
+      if (!validEmail.matches())
+        return "Email specified is not a valid email.";
+      else
+        return null;
+    }
+
+    public boolean isPassword(String potentialPassword) {
+      return BCrypt.checkpw(potentialPassword, password);
+    }
+
+    public void changePassword(String newPassword) {
+      password = BCrypt.hashpw(newPassword, BCrypt.gensalt());
+    }
 }
