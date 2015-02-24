d2c371d3a2c4f33d8cdec3dbcb9b1c470d79b255
Philip Wernersbach <philip.wernersbach@gmail.com>
2015-02-23 20:08:07 -0500

    Fixup Item model.

--- src/app/models/Item.java
+++ src/app/models/Item.java
@@ -7,47 +7,25 @@ import play.data.validation.*;
 import play.db.ebean.*;
 import com.avaje.ebean.*;
 
-import controllers.BCrypt;
-
 @Entity
-public class User extends Model {
-    /* Email regex from http://www.mkyong.com/regular-expressions/how-to-validate-email-address-with-regular-expression/ */
-    private static final String EMAIL_REGEX = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
-
+public class Item extends Model {
     @Id
     public Integer itemId;
 
     @Constraints.Required
     public String itemName;
-	@Constraints.Required
-	public String storeName;
+	  @Constraints.Required
+	  public String storeName;
     @Constraints.Required
     public Long storeZip;
 
     public Item(String item, String store, Long zip) {
       this.itemName = item;
-      this.sotreName = store;
+      this.storeName = store;
       this.storeZip = zip;
     }
 
-    public static Finder<String,User> find = new Finder<String,User>(
-        String.class, User.class
+    public static Finder<Integer,Item> find = new Finder<Integer,Item>(
+        Integer.class, Item.class
     );
-
-    public String validate() {
-      Matcher validEmail = EMAIL_PATTERN.matcher(email);
-      if (!validEmail.matches())
-        return "Email specified is not a valid email.";
-      else
-        return null;
-    }
-
-    public boolean isPassword(String potentialPassword) {
-      return BCrypt.checkpw(potentialPassword, password);
-    }
-
-    public void changePassword(String newPassword) {
-      password = BCrypt.hashpw(newPassword, BCrypt.gensalt());
-    }
 }
