80e66c98164723909f79c9bc1d51e32dd5487fa0
Philip Wernersbach <philip.wernersbach@gmail.com>
2015-02-25 22:18:23 -0500

    Add logout button and login status info.

--- src/app/controllers/Application.java
+++ src/app/controllers/Application.java
@@ -25,21 +25,32 @@ public class Application extends Controller {
         User user = User.find.byId(potentialUser.email);
 
         /* Check if correct password was supplied. */
-        if (!user.isPassword(potentialUser.password))
+        if (!user.isPassword(potentialUser.password)) {
+            flash("login_status", "Invalid username or password specified!");
             return badRequest(login.render(data, Form.form(User.class)));
+        }
 
         session().clear();
         session("user_email", user.email);
+
+
+        if ((flash("login_status") == "") || (flash("login_status") == null))
+            flash("login_status", "Successfully logged in!");
+        else
+            flash("login_status", "Successfully registered!");
+
         return redirect(routes.Application.user());
     }
 
     public static Result authenticate() {
         Form<User> data = Form.form(User.class).bindFromRequest();
 
-        if (data.hasErrors())
+        if (data.hasErrors()) {
+            flash("login_status", "Please enter data into all fields in the Login form.");
             return badRequest(login.render(data, Form.form(User.class)));
-        else
+        } else {
             return doLogin(data);
+        }
     }
 
     /* This is called for registering users. */
@@ -51,23 +62,29 @@ public class Application extends Controller {
         Form<User> data = Form.form(User.class).bindFromRequest();
 
         /* Error checking. */
-        if (data.hasErrors())
+        if (data.hasErrors()) {
+            flash("login_status", "Please enter data into all fields in the Register form.");
             return badRequest(login.render(Form.form(User.class), data));
+        }
 
         /* Get data from form. */
         newUser = data.get();
 
         /* Check to see if there's already a user for this email. */
-        if (User.find.byId(newUser.email) != null)
+        if (User.find.byId(newUser.email) != null) {
+            flash("login_status", "Account already exists for email!");
             return badRequest(login.render(Form.form(User.class), data));
+        }
 
         /* Change user password and save to database. */
         newUser.changePassword(newUser.password);
         newUser.save();
 
+        flash("login_status", "Successfully registered!");
+
         return doLogin(data);
     }
-    
+
     /*this is called then user hit update button*/
     public static Result updateUser(){
     	User existingUser = null;
@@ -80,12 +97,12 @@ public class Application extends Controller {
 
         /* Get data from form. */
         existingUser = data.get();
-        
+
         /* Change user password and save to database. */
         existingUser.changePassword(existingUser.password);
         existingUser.changeName(existingUser.name);
         existingUser.save();
-		return user();    	
+		return user();
     }
 
     public static Result logout() {
